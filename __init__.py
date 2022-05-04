ac72d0842da73c62a97ad04bead73f491b349b06
mmd <abusemmd@gmail.com>
2022-05-04 15:47:01 +0430

    import ease

---
+++ __init__.py
@@ -0,0 +1,14 @@
+from .update import Update
+from .bot import Bot
+from .handlers.filter import Filter
+from .handlers.handler import Handler
+from .utils.context import Context
+from .utils.utils import Req
+__all__ = (
+    Update,
+    Bot,
+    Filter,
+    Handler,
+    Context,
+    Req
+)
---
+++ objects/__init__.py
@@ -0,0 +1,8 @@
+from .keyboard import inline_button, inline_markup, keyboard_button, keyboard_markup
+
+__all__ = (
+    inline_markup,
+    inline_button,
+    keyboard_button,
+    keyboard_markup
+)
