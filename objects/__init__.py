3da77a3a449c43a2b375d610fce359c4b49293a7
mmd <abusemmd@gmail.com>
2022-06-27 10:51:15 +0430

    importing fix

--- objects/__init__.py
+++ objects/__init__.py
@@ -1,8 +1,17 @@
 from .keyboard import inline_button, inline_markup, keyboard_button, keyboard_markup
+from .audio import Audio
+from .chat import Chat
+from .user import User
+from .video import Video
+from .voice import Voice
 
 __all__ = (
     inline_markup,
     inline_button,
     keyboard_button,
-    keyboard_markup
+    keyboard_markup,
+    Audio,
+    Chat,
+    User,
+    Video
 )
